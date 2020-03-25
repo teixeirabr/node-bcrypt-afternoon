2bce8e5d0c1c73914311e820d7764a80db99ee2a
andre <andrelaprov@gmail.com>
2020-03-25 09:04:55 -0600

    server fixed

--- server/index.js
+++ server/index.js
@@ -9,4 +9,24 @@ const app = express();
 
 app.use(express.json());
 
+const { SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false }
+})
+  .then(db => {
+    app.set("db", db);
+    console.log("Db Connected :) !!!");
+  })
+  .catch(err => console.log(err));
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+  })
+);
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
